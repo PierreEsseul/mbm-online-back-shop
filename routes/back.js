884d78c4be79d1c6af621e0ac38bb737dbdf7080
Thomas Viot <viotthomas7@gmail.com>
2023-02-08 12:23:23 +0100

    back pour stripe

--- routes/back.js
+++ routes/back.js
@@ -1,16 +1,8 @@
 import express from "express";
-
+import Stripe from "stripe"
 import shop from "../src/onlineShop.js";
-import cors from 'cors';
-import {Stripe} from 'stripe'
 
 const router = express.Router();
-
-const stripe = new Stripe(`${process.env.STRIPE_PRIVATE_KEY}`, {
-	apiVersion: "2022-07-27"
-  });
-
-console.log(stripe);
 
 router.get('/', async (req, res) => { 
     const { slugify } = req.query;
@@ -28,28 +20,43 @@ router.get('/', async (req, res) => {
     res.send({ success: true, shop: ret });
 });
 
-router.post("/payment", cors(), async (req, res) => {
-	let { amount, id } = req.body
-	try {
-		const payment = await stripe.paymentIntents.create({
-			amount,
-			currency: "USD",
-			description: "Spatula company",
-			payment_method: id,
-			confirm: true
-		})
-		console.log("Payment", payment)
-		res.json({
-			message: "Payment successful",
-			success: true
-		})
-	} catch (error) {
-		console.log("Error", error)
-		res.json({
-			message: "Payment failed",
-			success: false
-		})
-	}
-})
+// Stripe 
+
+const stripe = new Stripe(`${process.env.STRIPE_PRIVATE_KEY}`)
+
+
+router.post("/payment", async (req, res) => {
+ const { items, amount } = req.body;
+ 
+ const paymentIntent = await stripe.paymentIntents.create({ 
+   amount,
+   currency: "eur",
+   automatic_payment_methods: {
+     enabled: true,
+   },
+ });
+ 
+ res.send({
+   clientSecret: paymentIntent.client_secret,
+ });
+});
+
+
+// router.post("/payment", async (req, res) => {
+// 	let { amount, id } = req.body
+// 	console.log('/payment : ', { amount, id });
+
+// 		const payment = await stripe.paymentIntents.create({
+// 			amount,
+// 			currency: "USD",
+// 			description: "Spatula company",
+// 			payment_method: id,
+// 			confirm: true
+// 		})
+// 		console.log("Payment", payment)
+// 		res.send({
+// 			clientSecret: paymentItent.clientSecret,
+// 		})
+// })
 
 export default router;
